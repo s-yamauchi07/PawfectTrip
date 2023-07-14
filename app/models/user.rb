71936c618e5d42d8ef9e92ceffe808ef6daef301
s-yamauchi07 <hand1nhand306@gmail.com>
2023-07-14 20:47:58 +0900

    ゲストユーザー機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: {with: VALID_PASSWORD_REGEX, message: "must contain both letters and at least one number."}
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.nickname = 'GUEST'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
