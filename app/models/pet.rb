c4e6bc0dae1c62af867dea2ef8aa22ac2597d021
s-yamauchi07 <hand1nhand306@gmail.com>
2023-07-14 21:04:26 +0900

    petモデルの作成

---
+++ app/models/pet.rb
@@ -0,0 +1,11 @@
+class Pet < ApplicationRecord
+  belongs_to :user, optional: true
+  
+  with_options presence: true do
+    validates :name
+    validates :breed
+    validates :birthday
+  end
+
+  validates :size_id, numericality: { other_than: 0 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_080120) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_14_115639) do
+  create_table "pets", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "breed", null: false
+    t.integer "size_id", null: false
+    t.date "birthday", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_080120) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "users"
 end
