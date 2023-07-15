7abf4a267fb61ae95d814f84de32455f44570171
s-yamauchi07 <hand1nhand306@gmail.com>
2023-07-15 11:08:18 +0900

    user新規登録のウィザード化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,62 +8,40 @@ class Users::RegistrationsController < Devise::RegistrationsController
       redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
     end
   end
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    #@user = User.newと同意。
+    super
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(sign_up_params)
+      unless @user.valid?
+        render :new, status: :unporcessable_entity and return
+      end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @pet = @user.pets.build
+    render :new_pet, status: :accepted
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def create_pet
+    @user = User.new(session["devise.regist_data"]["user"])
+    @pet = Pet.new(pet_params)
+      unless @pet.valid?
+        render :new_pet, status: :unporcessable_entity and return
+      end
+    @user.pets.build(@pet.attributes)
+    @user.save
+    session["devise.regist_data"]["user"].clear
+    sign_in(:user, @user)
+    redirect_to root_path
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  private
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :size_id, :birthday)
+  end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get 'pets', to: 'users/registrations#new_pet'
+    post 'pets', to: 'users/registrations#create_pet'
   end
   root "plans#index"
 end
