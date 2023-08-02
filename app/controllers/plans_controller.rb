2baa510f23eb6625414143c9a02d710e4c75d0ff
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-02 12:43:52 +0900

    planコントローラーの作成

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,24 @@
 class PlansController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      render :create, status: :unprocessable_entity
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def plan_params
+    params.require(:plan).permit(:title, :departure_date, :return_date,:departure_id, :destination_id,:companion_id, :dog_id).merge(user_id: current_user.id)
   end
 end
