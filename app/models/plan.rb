df8d98a9b1996eae23a26aa8502a4b2f1a84aa35
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-02 12:43:43 +0900

    planモデルの作成

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,11 +4,11 @@ class Plan < ApplicationRecord
   with_options presence: true do
     validates :title 
     validates :departure_date
-    validates :destination_date
+    validates :return_date
     validates :departure_id
     validates :destination_id
     validates :companion_id
     validates :dog_id
   end
-  
+
 end
