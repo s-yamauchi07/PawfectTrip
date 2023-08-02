04c267a3e262655d628077d385e80328823a29d9
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-02 11:36:10 +0900

    ActiveHashモデルの追加

---
+++ app/models/companion.rb
@@ -0,0 +1,12 @@
+class Companion < ActiveHash::Base
+  self.data = [
+    {id: 1, name: '無し(一人)'},
+    {id: 2, name: '友人'},
+    {id: 3, name: '夫婦・パートナー'},
+    {id: 4, name: '家族(子連れ)'},
+    {id: 3, name: 'その他'},
+  ]
+
+  include ActiveHash::Associations
+  has_many :plans
+end
