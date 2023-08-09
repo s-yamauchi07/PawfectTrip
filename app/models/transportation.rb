90a6111a3ba220ada9a38f1236d2ee804bd1c363
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-09 12:49:27 +0900

    行程入力画面のビュー修正

--- app/models/transportation.rb
+++ app/models/transportation.rb
@@ -1,11 +1,11 @@
 class Transportation < ActiveHash::Base
   self.data = [
-    {id: 1, name: '徒歩'},
-    {id: 2, name: '自転車'},
-    {id: 3, name: '車'},
-    {id: 4, name: '電車'},
-    {id: 5, name: '飛行機'},
-    {id: 6, name: '船'},
+    {id: 1, name: '徒歩', image: "walk.png"},
+    {id: 2, name: '自転車',image: "bicycle.png"},
+    {id: 3, name: '車',image: "car.png"},
+    {id: 4, name: '電車',image: "train.png"},
+    {id: 5, name: '飛行機',image: "airplain.png"},
+    {id: 6, name: '船',image: "ship.png"},
   ]
 
   include ActiveHash::Associations
