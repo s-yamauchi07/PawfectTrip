4a4b8a720408315a050f41d02e9cda1eed92ca9a
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-16 00:49:19 +0900

    plansテーブルの変更

--- db/migrate/20230802022136_create_plans.rb
+++ db/migrate/20230802022136_create_plans.rb
@@ -3,11 +3,12 @@ class CreatePlans < ActiveRecord::Migration[7.0]
     create_table :plans do |t|
       t.string   :title,            null: false
       t.datetime :departure_date,   null: false
-      t.datetime :return_date, null: false
+      t.datetime :return_date,      null: false
       t.integer :departure_id,      null: false
       t.integer :destination_id,    null: false
       t.integer :companion_id,      null: false
-      t.references :dog,          null: false
+      t.references :dog,            null: false
+      t.string :tag,                null: false
       t.references :user,           null: false
       t.timestamps
     end
