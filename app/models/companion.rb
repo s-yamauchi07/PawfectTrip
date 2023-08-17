d35805bccdc14bf325aeb934bf94099190380d73
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-17 20:08:37 +0900

    ActiveHashカラムの変更

--- app/models/companion.rb
+++ app/models/companion.rb
@@ -1,10 +1,11 @@
 class Companion < ActiveHash::Base
   self.data = [
-    {id: 1, name: '無し(一人)'},
-    {id: 2, name: '友人'},
-    {id: 3, name: '夫婦・パートナー'},
-    {id: 4, name: '家族(子連れ)'},
-    {id: 5, name: 'その他'},
+    {id: 1, name: '---' },
+    {id: 2, name: '無し(一人)'},
+    {id: 3, name: '友人'},
+    {id: 4, name: '夫婦・パートナー'},
+    {id: 5, name: '家族(子連れ)'},
+    {id: 6, name: 'その他'},
   ]
 
   include ActiveHash::Associations
