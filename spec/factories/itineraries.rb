8e7596ec43611db43208b676091b22397b1e0fe0
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-18 11:54:35 +0900

    itineraryモデルのテストコード

--- spec/factories/itineraries.rb
+++ spec/factories/itineraries.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :itinerary do
-    
+    date               { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}
+    place              { Faker::Lorem.word }
+    transportation_id  { Faker::Number.within(range: 1..6)}
+
+    association :plan
   end
 end
