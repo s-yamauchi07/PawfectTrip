9a402fa263765b652664e4f5302ccc5fbe29fa1f
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-22 11:31:37 +0900

    画像のPreview機能

---
+++ app/javascript/controllers/preview_image_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="preview-image"
+export default class extends Controller {
+  static targets = ["file", "image"]
+  preview() {
+    this.fileTarget.addEventListener('change', (e) =>{
+      const preview = this.imageTarget
+      const file = e.target.files[0]
+
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const dataURL = e.target.result;
+        const blobImage = document.createElement('img')
+        blobImage.setAttribute('src',dataURL);
+        blobImage.setAttribute('style',"width: 100%");
+        preview.append(blobImage)
+      }
+      fileReader.readAsDataURL(file)
+    })
+  }
+}
