a475ff6d291d7b8bc2d1d06981474b9ebc4a7cdd
s-yamauchi07 <hand1nhand306@gmail.com>
2023-08-23 12:52:04 +0900

    ページネーション機能の追加

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,7 +2,7 @@ class PlansController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @plans = Plan.order("created_at DESC")
+    @plans = Plan.page(params[:page]).per(8).order("created_at DESC")
   end
 
   def new
