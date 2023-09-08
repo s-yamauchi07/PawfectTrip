88aa239bc7aa329c474dac9910aa50386fa7827b
s-yamauchi07 <hand1nhand306@gmail.com>
2023-09-08 13:25:21 +0900

    編集処理の実装

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,6 @@
 class PlansController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_plan, only: [:show, :edit, :update]
 
   def index
     @plans = Plan.page(params[:page]).per(8).order("created_at DESC")
@@ -23,16 +24,33 @@ class PlansController < ApplicationController
   end
 
   def show
-    @plan = Plan.find(params[:id])
     gon.spots = @plan.itineraries
     # @planに紐づくitinerariesを日付でグループ分けする
     @itineraries = @plan.itineraries.all.group_by { |itinerary| itinerary.date.strftime("%m/%d")}
     @unique_date = @itineraries.keys.group_by {|date| date}.uniq
   end
 
+  def edit
+  end
+
+  def update
+    # binding.pry
+    if @plan.update(plan_params)
+      redirect_to plan_path(@plan)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
  
   def plan_params
-    params.require(:plan).permit(:cover_image ,:title, :departure_date, :return_date,:departure_id, :destination_id,:companion_id, :pet_id, itineraries_attributes:[:date, :place, :transportation_id, :memo, :lat, :lng]).merge(user_id: current_user.id)
+    params.require(:plan).permit(:cover_image ,:title, :departure_date, :return_date,:departure_id, :destination_id,:companion_id, :pet_id, itineraries_attributes:[:id, 
+      
+      :date, :place, :transportation_id, :memo, :lat, :lng, :_destroy]).merge(user_id: current_user.id)
+  end
+
+  def set_plan
+    @plan = Plan.find(params[:id])
   end
 end
