ff3745ef46ca1c7b7f2d90e160afee70d4b4d92b
s-yamauchi07 <hand1nhand306@gmail.com>
2023-09-28 10:47:47 +0900

    hotelモデルの作成

--- db/migrate/20230928010300_create_hotels.rb
+++ db/migrate/20230928010300_create_hotels.rb
@@ -1,11 +1,6 @@
 class CreateHotels < ActiveRecord::Migration[7.0]
   def change
     create_table :hotels do |t|
-      t.string    :name,       null: false
-      t.string    :prefecture, null: false
-      t.string    :address,    null: false
-      t.float     :lat,        null: false
-      t.float     :lng,        null: false
       t.integer   :hotel_num,  null: false
       t.timestamps
     end
