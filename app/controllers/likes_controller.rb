0eed905e488c9516a0439a04f90cfff3505e189e
s-yamauchi07 <hand1nhand306@gmail.com>
2023-10-08 12:05:29 +0900

    likes/create, destroy アクションの作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,28 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_plan
 
   def create
-    binding.pry
+    current_user.plan_likes.create(plan_id: @plan.id)
+    render turbo_stream: turbo_stream.replace(
+      "plan-like",
+      partial: "likes/like",
+      locals: { plan: @plan }
+    )
   end
 
   def destroy
+    plan_like = current_user.plan_likes.find_by(plan_id: @plan)
+    plan_like.destroy
+    render turbo_stream: turbo_stream.replace(
+      "plan-like",
+      partial: "likes/like",
+      locals: { plan: @plan }
+    )
   end
 
   private
   def set_plan
-    
+    @plan = Plan.find(params[:plan_id])
   end
 end
