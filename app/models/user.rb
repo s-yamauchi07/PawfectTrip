60a06ba52526b1a805ee3dd178f996dd2fb23bb4
s-yamauchi07 <hand1nhand306@gmail.com>
2023-10-08 12:14:00 +0900

    アソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
 
   has_many :pets
   has_many :hotel_likes
-  has_many :likes, dependent: :destroy
   has_many :plan_likes
+  has_many :plans, dependent: :destroy
   validates :nickname, presence:true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ーa-zA-Z0-9]+\z/} 
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
