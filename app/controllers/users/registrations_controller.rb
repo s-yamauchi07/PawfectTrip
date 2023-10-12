f2b84412feabc2e68e9b339c1fd131054fe34ccf
s-yamauchi07 <hand1nhand306@gmail.com>
2023-10-12 13:09:12 +0900

    ユーザー更新機能の実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_nomal_user, only: [:update, :destroy]
-
-  def ensure_normal_user
-    if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
-    end
-  end
+  before_action :ensure_normal_user, only: [:update, :destroy]
 
   def new
     #@user = User.newと同意。
@@ -21,8 +15,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
     session["devise.regist_data"] = {user: @user.attributes}
     session["devise.regist_data"][:user]["password"] = params[:user][:password]
-    @pet = @user.pets.build
+    @pet = @user.pet.build
     render :new_pet, status: :accepted
+  end
+
+  def edit
+    super
+  end
+
+  def update
+    @user = current_user
+    unless @user.update(account_update_params)
+      render :edit, status: :unprocessable_entity and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @pet = Pet.find_by(user_id: @user.id)
+    render :edit_pet, status: :accepted
   end
 
   def create_pet
@@ -31,17 +40,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
       unless @pet.valid?
         render :new_pet, status: :unprocessable_entity and return
       end
-    @user.pets.build(@pet.attributes)
+    @user.pet.build(@pet.attributes)
     @user.save
     session["devise.regist_data"]["user"].clear
     sign_in(:user, @user)
     redirect_to root_path
   end
 
+  def update_pet
+    @user = User.find(session["devise.regist_data"]["user"]["id"])
+    @pet = Pet.find_by(user_id: @user.id)
+    unless @pet.update(pet_params)
+      render :edit_pet, status: :unprocessable_entity and return
+    end
+    sign_in(:user, @user)
+    redirect_to user_path(@user)
+    
+  end
+
   private
 
   def pet_params
     params.require(:pet).permit(:name, :breed, :size_id, :birthday)
+  end
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
+    end
   end
 
 end
