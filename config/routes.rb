63e62fce486358ac6433bdcd77d07769b40b3ba4
s-yamauchi07 <hand1nhand306@gmail.com>
2023-10-13 12:46:20 +0900

    退会処理を実行するルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   }
   root "plans#index"
   resources :users, only: [:show]
+  get "users/:id/unsubscribe", to: "users#unsubscribe", as: "unsubscribe"
+  patch "users/:id/withdraw", to: "users#withdraw", as: "withdraw"
+  
   resources :hotels, only: [:show,:create, :destroy] do
     collection do
       get 'search'
@@ -17,6 +20,7 @@ Rails.application.routes.draw do
     post 'pets', to: 'users/registrations#create_pet'
     patch 'pets/:id', to: 'users/registrations#update_pet',as: :update_pet
   end
+  # 退会用ページを表示するためのルーティング
 
   resources :plans do
     resources :itineraries, only:[:new, :create]
