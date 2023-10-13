d70a14aca88a712363eddd01024418b4c6f71476
s-yamauchi07 <hand1nhand306@gmail.com>
2023-10-13 12:46:41 +0900

    アソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,14 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :size
 
-  has_many :pet
+  has_one :pet
   has_many :hotel_likes
   has_many :plan_likes
   has_many :plans, dependent: :destroy
-  validates :nickname, presence:true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ーa-zA-Z0-9]+\z/} 
+  validates :nickname, presence:true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ーa-zA-Z0-9]+\z/}
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: {with: VALID_PASSWORD_REGEX, message: "must contain both letters and at least one number."}
+  validates :password, format: {with: VALID_PASSWORD_REGEX, message: "must contain both letters and at least one number."}, on: :create
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
