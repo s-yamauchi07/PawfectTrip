b723f80ea160f1b6cc4b22c95912f550381982cd
s-yamauchi07 <hand1nhand306@gmail.com>
2023-10-13 12:46:29 +0900

    退会処理のアクション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!, only: [:show, :withdraw]
   
   def show
     @user = User.find(params[:id])
+  end
+
+  def withdraw
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理が完了しました"
+    redirect_to root_path
   end
 
 end
