4ae2c658dc218776a2753105dd5df7d2ffab81a8
s-yamauchi07 <hand1nhand306@gmail.com>
2023-10-25 11:04:43 +0900

    お気に入り項目のタブ化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+
+    hotel_likes = @user.hotel_likes
+    @hotel_list = fetch_hotels(hotel_likes)
   end
 
   def withdraw
@@ -11,6 +14,17 @@ class UsersController < ApplicationController
     reset_session
     flash[:notice] = "退会処理が完了しました"
     redirect_to root_path
+  end
+
+  private
+  def fetch_hotels(hotel_likes)
+    hotels = []
+    hotel_likes.each do |hotel_like|
+      # API呼び出しとデータの整形を行う
+      hotel_data = RakutenWebService::Travel::Hotel.search(hotelNo: Hotel.find(hotel_like.hotel_id).hotel_num, datumType: 1).first
+      hotels << hotel_data
+    end
+    return hotels
   end
 
 end
