17efabf650536f5dcd87981aaa7d2f089b949d2e
s-yamauchi07 <hand1nhand306@gmail.com>
2023-10-31 12:46:48 +0900

    コントローラーの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,37 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_plan, only: [:index, :create]
+  before_action :set_comments, only: [:index, :create]
+
+  def index
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+      respond_to do |format|
+        if @comment.save
+          # 入力フォームのリセットのために変数を再定義
+          @comment = Comment.new
+          format.html { redirect_to plan_comments_path }
+          format.turbo_stream
+        else
+          format.html { render :index, status: :unprocessable_entity }
+        end
+      end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, plan_id: params[:plan_id])
+  end
+
+  def set_plan
+    @plan = Plan.find(params[:plan_id])
+  end
+
+  def set_comments
+    @comments = @plan.comments.includes(:user)
+  end
 end
