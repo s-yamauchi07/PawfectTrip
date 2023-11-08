0fd601e4dcc7bdad45766c658f356c724882c54c
s-yamauchi07 <hand1nhand306@gmail.com>
2023-11-09 02:47:17 +0900

    actionケーブルの読み込み変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./map"import "./channels"
+import "./map"
+// import "./channels"
+import "@rails/actioncable";
