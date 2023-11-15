477431659b2132defe1b2d9de2e4139da073bfcf
s-yamauchi07 <hand1nhand306@gmail.com>
2023-11-15 11:43:32 +0900

    userモデルの更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:google_oauth2]
   attr_accessor :current_password
          
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -29,5 +29,22 @@ class User < ApplicationRecord
 
   def active_for_authentication?
     super && (is_deleted == false)
+  end
+
+  def self.from_omniauth(auth)
+    sns = SnsCreadential.where(provider: auth.provider, uid:auth.uid).first_or_create
+
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+
+    # persisted？でuserが既に登録済みか確認。登録済みuserの場合はsnscredentialテーブルのuser_idカラムと紐づける。
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+    
   end
 end
