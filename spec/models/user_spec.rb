cdacc9ffadf78cb17919a4462c4aa59e4c92d079
s-yamauchi07 <hand1nhand306@gmail.com>
2023-11-16 12:34:10 +0900

    userモデルの日本語化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,75 +16,75 @@ RSpec.describe User, type: :model do
       it 'nicknameが空の場合' do
         @user.nickname = ""
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
       end
       it 'nicknameが半角カタカナの場合' do
         @user.nickname = 'ﾀﾛｳ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname is invalid")
+        expect(@user.errors.full_messages).to include("ニックネームは不正な値です")
       end
       it 'nicknameに記号が含まれる場合' do
         @user.nickname = '太郎@'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname is invalid")
+        expect(@user.errors.full_messages).to include("ニックネームは不正な値です")
       end
       it 'nicknameに全角数字が含まれる場合' do
         @user.nickname = '太郎７７７'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname is invalid")
+        expect(@user.errors.full_messages).to include("ニックネームは不正な値です")
       end
       it 'nicknameに全角英字が含まれる場合' do
         @user.nickname = 'ｔａｒｏ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname is invalid")
+        expect(@user.errors.full_messages).to include("ニックネームは不正な値です")
       end
       it 'emailが空の場合' do
         @user.email = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
       end
       it 'emailが重複している場合' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+        expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
       end
       it 'emailに@が含まれない場合' do
         @user.email = "taro.test.com"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include("Eメールは不正な値です")
       end
       it 'passwordが空の場合' do
         @user.password = ""
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
       end
       it 'passwordが6文字未満の場合' do
         @user.password = "123ab"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
       end
       it 'passwordが数字のみの場合' do
         @user.password = "123456"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password must contain both letters and at least one number.")
+        expect(@user.errors.full_messages).to include("パスワードは半角英数字混合で入力してください")
       end
       it 'passwordが英字のみの場合' do
         @user.password = "abcdef"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password must contain both letters and at least one number.")
+        expect(@user.errors.full_messages).to include("パスワードは半角英数字混合で入力してください")
       end
       it 'passwordに全角文字が含まれる場合' do
         @user.password = "１２３abc"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password must contain both letters and at least one number.")
+        expect(@user.errors.full_messages).to include("パスワードは半角英数字混合で入力してください")
       end
       it 'passwordと確認用passwordが一致しない場合' do
         @user.password = "abc123"
         @user.password_confirmation = "123abc"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
     end
   end
