c564cf1c09c2019024c4278654468c82b8c3374b
s-yamauchi07 <hand1nhand306@gmail.com>
2023-11-17 12:49:51 +0900

    タグモデルの単体テスト

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :tag do
-    
+    tag_name { Faker::Lorem.words(number: 1) }
   end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tag = FactoryBot.build(:tag)
+  end
+
+  describe 'タグ新規登録' do
+    context 'タグの新規登録ができるとき' do 
+      it '新規のtagの場合' do
+        expect(@tag).to be_valid
+      end
+    end
+
+    context 'タグの新規登録ができないとき' do 
+      it 'タグが重複している場合' do
+        @tag.save
+        second_tag = FactoryBot.build(:tag)
+        second_tag.tag_name = @tag.tag_name
+        second_tag.valid?
+        expect(second_tag.errors.full_messages).to include("タグはすでに存在します")
+      end
+    end
+  end
 end
