9707f6bb64645569bbb2b17e54205bb4a8c87845
s-yamauchi07 <hand1nhand306@gmail.com>
2024-01-28 14:35:28 +0900

    鍵の修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,4 +28,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/perfecttrip_ssh.pem'] 
+                                  keys: ['~/.ssh/pawfecttrip.pem'] 
