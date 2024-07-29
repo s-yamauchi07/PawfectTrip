8171df4581a8363e764490e25748aa77b0020226
s-yamauchi07 <hand1nhand306@gmail.com>
2024-07-29 18:14:34 +0900

    google autocompleteの修正

--- app/javascript/controllers/place_search_controller.js
+++ app/javascript/controllers/place_search_controller.js
@@ -3,7 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="place-search"
 export default class extends Controller {
   static targets = ["place", "lat", "lng"]
+
   search() {
+    // GoogleAPIの読み込みを確認
+    if (typeof google === 'undefined' || typeof google.maps === 'undefined' || typeof google.maps.places === 'undefined') {
+      console.error("Google Maps API not loaded");
+      return;
+    }
+    
     const input = this.placeTarget;
     const lat = this.latTarget;
     const lng = this.lngTarget;
