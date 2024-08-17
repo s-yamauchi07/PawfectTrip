c89da57346a2f97423ab08b47a1db58720810fff
s-yamauchi07 <hand1nhand306@gmail.com>
2024-08-17 23:51:00 +0900

    宿泊先検索ページのレスポンシブ対応

--- app/javascript/controllers/search_hotel_controller.js
+++ app/javascript/controllers/search_hotel_controller.js
@@ -72,17 +72,14 @@ export default class extends Controller {
         <div class="card max-w-sm bg-base-100 shadow-xl">
           <div class="relative">
             <figure>
-              <img src=${hotelImage} alt="hotel-image" onerror="this.src='${altImageUrl}';" class="h-80 w-full object-cover rounded-t-2xl" />
+              <img src=${hotelImage} alt="hotel-image" onerror="this.src='${altImageUrl}';" class="h-48 w-full object-cover rounded-t-2xl" />
             </figure>
           </div>
           <div class="card-body">
-            <h3 class="card-title">
+            <h3 class="text-lg lg:text-xl font-semibold">
             <a href="${hotelurl}">${hotelName}</a>
             </h3>
-            <p class="flex h-12 items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-              <path fill-rule="evenodd" d="m9.69 18.933.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 0 0 .281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 1 0 3 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 0 0 2.273 1.765 11.842 11.842 0 0 0 .976.544l.062.029.018.008.006.003ZM10 11.25a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Z" clip-rule="evenodd" />
-              </svg>
+            <p class="text-sm lg:text-base text-gray-500">
               ${hoteladdress}
             </p>
             <div class="card-actions justify-end">
